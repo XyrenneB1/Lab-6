0419e642e20c0c10363f361e6ee6d5bf273fd8ef
XyrenneB1 <xyrennethessamae.bastian@edu.sait.ca>
2026-03-24 12:28:15 -0600

    created variables at js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,4 @@
-/****************** YOUR NAME: 
+/****************** YOUR NAME: Xyrenne Thessa Mae Bastian
 
 The instructions describe the missing logic that is needed; you will translate these into JavaScript in the places indicated.
 
@@ -11,7 +11,8 @@ You are encouraged to use the provided naming convention for ease of review.
 
 // INSERT YOUR CODE HERE
 
-
+let modelName = "XYZ":
+let duration = 0;
 
 
 
@@ -27,7 +28,11 @@ You are encouraged to use the provided naming convention for ease of review.
 
 // INSERT YOUR CODE HERE
 
-
+function recalculate() {
+    let costLabel = document.getElementById("calculated-cost0");
+    let totalCost = 0;
+    if ()
+}
 
 
 
